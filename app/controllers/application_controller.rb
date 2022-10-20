f708e9fb6f9b451b860194ea6f6e6e5b2617d5ea
bdc7ba <bcrozier@chicagobooth.edu>
2022-10-20 23:11:34 +0000

    square totally working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,20 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_form.html.erb"})
   end
 
+  def calculate_square
+    #params = {"square" => 42}
+    @num = params.fetch("square").to_f
+    @square_of_num = @num**2
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
   def blank_root_form
     
     render({ :template => "calculation_templates/root_form.html.erb"})
+  end
+
+  def calculate_root
+    render({ :template => "calculation_templates/root_results.html.erb"})
   end
 
   def blank_payment_form
@@ -14,8 +25,18 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/payment_form.html.erb"})
   end
 
+  def calculate_payment
+    render({ :template => "calculation_templates/payment_results.html.erb"})
+  end
+  
+
   def blank_random_form
   
     render({ :template => "calculation_templates/random_form.html.erb"})
   end
+
+  def calculate_random
+    render({ :template => "calculation_templates/random_results.html.erb"})
+  end
+  
 end
